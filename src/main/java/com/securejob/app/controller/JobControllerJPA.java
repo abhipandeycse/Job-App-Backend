a84e278838edb8b021596ff04941fec5bb99c0cc
Abhishek Pandey <abhi.pandeycse@gmail.com>
2024-09-17 02:22:12 +0530

    first commit

--- src/main/java/com/securejob/app/controller/JobControllerJPA.java
+++ src/main/java/com/securejob/app/controller/JobControllerJPA.java
@@ -1,4 +1,4 @@
-package com.securejob.app;
+package com.securejob.app.controller;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/securejob/app/controller/JobUserRegisterController.java
+++ src/main/java/com/securejob/app/controller/JobUserRegisterController.java
@@ -1,4 +1,4 @@
-package com.securejob.app;
+package com.securejob.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
